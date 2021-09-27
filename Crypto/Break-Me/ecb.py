5f4fffe9a38532bd5bdb1b2d238c7d311d9d2056
Quang Vo <vothanhtruongquang@gmail.com>
2021-09-27 11:01:07 +0700

    update reconnect

--- Crypto/Break-Me/ecb.py
+++ Crypto/Break-Me/ecb.py
@@ -32,34 +32,36 @@ def find_offset(p) -> int:
 def brute_force_letter(p, key="") -> str:
     offset = find_offset(p)
     offset_str = "B" * offset
+    try:
+        for i in range(0, block_size):
+            static = "A"*(block_size - len(key) - 1)
+            base_block = send_payload(p, offset_str + static)
+            base_block = b64decode(base_block[0])
+            block_should_be = base_block[block_size*2: block_size*3]
 
-    for i in range(0, block_size):
-        static = "A"*(block_size - len(key) - 1)
-        base_block = send_payload(p, offset_str + static)
-        base_block = b64decode(base_block[0])
-        block_should_be = base_block[block_size*2: block_size*3]
-
-        for c in string.printable:
-            data_send = offset_str + static + key + c
-            data_return = send_payload(p, data_send)[0]
-            decoded_block = b64decode(data_return)
-            if block_should_be == decoded_block[block_size*2:block_size*3]:
-                print ("Found a character: ", c)
-                key += c
-                print ("Current key value: ", key)
-                break
-
-    return key
+            for c in string.printable:
+                data_send = offset_str + static + key + c
+                data_return = send_payload(p, data_send)[0]
+                decoded_block = b64decode(data_return)
+                if block_should_be == decoded_block[block_size*2:block_size*3]:
+                    print ("Found a character: ", c)
+                    key += c
+                    print ("Current key value: ", key)
+                    break
+        return key
+        
+    except Exception:
+        p.close()
+        print ("Reconnecting ....")
+        new_process = remote("pwn-2021.duc.tf", 31914)
+        brute_force_letter(new_process, key)
 
 def main(p):
-    try:
-        key = brute_force_letter(p)
-        data = send_payload(p, "")
-        cipher = AES.new(key, AES.MODE_ECB)
-        pt = cipher.decrypt(data[0])
-        print(pt)
-    except Exception:
-  
+    key = brute_force_letter(p)
+    data = send_payload(p, "")
+    cipher = AES.new(key, AES.MODE_ECB)
+    pt = cipher.decrypt(data[0])
+    print(pt)
     # DUCTF{ECB_M0DE_K3YP4D_D474_L34k}
 
 
